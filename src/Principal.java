34b73819f3923d377c41c17c5241d245084f9fae
Manuela <manuelacavalcanti3@gmail.com>
2024-06-26 13:08:39 -0300

    Estudando orientação a objetos

--- src/Principal.java
+++ src/Principal.java
@@ -2,8 +2,10 @@ public class Principal {
     public static void main(String[] args) {
         Filme meuFilme = new Filme();
         meuFilme.nome = "A ponte para terabítia";
-        meuFilme.anoDeLancamento = 1970;
+        meuFilme.anoDeLancamento = 2007;
         meuFilme.duracaoEmMinutos = 190;
+
+        meuFilme.exibeFichaTecnica();
 
     }
 }
